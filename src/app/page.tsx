a2855c2d1f4e37ce2aebf8f94716ec7b07ea1c35
R Aswin <aswinr242004@gmail.com>
2024-06-02 14:10:27 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,6 @@ import { WagmiProvider } from 'wagmi'
 import { mainnet, polygon, sepolia } from 'wagmi/chains'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { getDefaultConfig } from '@rainbow-me/rainbowkit'
-import './globals.css'
-
 const config = getDefaultConfig({
   appName: 'MetaCert',
   projectId: 'YOUR_PROJECT_ID',
